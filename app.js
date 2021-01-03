fcd0489fac3a8ce09711d8127cc4bb8cae15b4f5
ddssads <tw0937608360@gmail.com>
2021-01-03 16:37:21 +0800

    project init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+const PORT = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost${PORT}`)
+})
